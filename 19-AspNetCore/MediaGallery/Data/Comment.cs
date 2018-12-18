d0f39e4ea5bc16410c1615edca2455086e3cd60b
estimon <33309282+estimon@users.noreply.github.com>
2018-12-18 11:33:40 +0200

    65465465456

---
+++ 19-AspNetCore/MediaGallery/Data/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace MediaGallery.Data
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public MediaItem Mediaitem { get; set; }
+        public IdentityUser User { get; set; }
+        public string Content { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Martin Ploompuu töö3/ContactAPI/Migrations/201812050905107_InitialCreate1.cs
@@ -0,0 +1,32 @@
+namespace ContactAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Type = c.String(),
+                        Value = c.String(),
+                        PersonId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.People", t => t.PersonId, cascadeDelete: true)
+                .Index(t => t.PersonId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contacts", "PersonId", "dbo.People");
+            DropIndex("dbo.Contacts", new[] { "PersonId" });
+            DropTable("dbo.Contacts");
+        }
+    }
+}
